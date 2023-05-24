70a4502f6064fb2816fc6814723506536af078da
Lina Petersson <lipe2106@student.miun.se>
2023-05-24 09:02:40 +0200

    Skapat grund för webpart

---
+++ src/webparts/searchInShareDocs/components/ISearchInShareDocsProps.ts
@@ -0,0 +1,7 @@
+export interface ISearchInShareDocsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/searchInShareDocs/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISearchInShareDocsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SearchInShareDocsWebPartStrings' {
+  const strings: ISearchInShareDocsWebPartStrings;
+  export = strings;
+}
